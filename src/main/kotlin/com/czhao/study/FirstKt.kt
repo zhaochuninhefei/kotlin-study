55b633d38fae4cdbbdcd2b67ae39d74146dc721b
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-10-14 18:06:29 +0800

    为FirstKt添加代码

--- src/main/kotlin/com/czhao/study/FirstKt.kt
+++ src/main/kotlin/com/czhao/study/FirstKt.kt
@@ -14,10 +14,16 @@ fun main(args: Array<String>) {
 
     print("sum2(1, 4) : ")
     println(sum2(1, 4))
+
+    print("printSum(1, 4) : ")
+    printSum(1, 4)
+
+    print("printSum2(1, 4) : ")
+    printSum2(1, 4)
 }
 
 /**
- * Sum1
+ * Sum1 显式声明返回类型的函数
  *
  * @param a
  * @param b
@@ -29,12 +35,29 @@ fun sum1(a: Int, b: Int): Int {
 
 
 /**
- * Sum2
+ * Sum2 没有函数体，直接用表达式赋值的函数。
+ * 其返回类型能够从表达式推断出来，可以不显式声明。(但显式声明也可以。)
  *
  * @param a
  * @param b
  */
 fun sum2(a: Int, b: Int) = a + b
 
+/**
+ * Print sum 没有返回值的函数，可以显式声明返回类型为Unit，也可以不声明。
+ *
+ * @param a
+ * @param b
+ */
+fun printSum(a: Int, b: Int) {
+    print("sum of $a and $b is ${a + b}\n")
+}
 
+/**
+ * Print sum2 没有返回值的函数，也可以用表达式赋值。
+ *
+ * @param a
+ * @param b
+ */
+fun printSum2(a: Int, b: Int) = print("sum of $a and $b is ${a + b}\n")
 
