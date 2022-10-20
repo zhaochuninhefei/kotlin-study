b20c06b65baa9c20e300ba5a1b5f78cb570c38ab
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-10-20 15:05:05 +0800

    map相关习惯写法

--- src/main/kotlin/com/czhao/study/lesson02/Lesson2.kt
+++ src/main/kotlin/com/czhao/study/lesson02/Lesson2.kt
@@ -3,6 +3,7 @@ package com.czhao.study.lesson02
 fun main() {
     testCustomer()
     testDefaultParam()
+    testMap()
 }
 
 fun testCustomer() {
@@ -29,4 +30,22 @@ fun testDefaultParam() {
 fun foo(a: Int = 1, b:String = "") {
     println("a is $a")
     println("b is $b")
+}
+
+fun testMap() {
+    println()
+    println("--- testMap ---")
+    val map = mapOf("李白" to 1, "白居易" to 3, "杜甫" to 2)
+    val v1 = map["李白"]
+    println("李白: $v1")
+    for ((name, order) in map) {
+        println("$name -> $order")
+    }
+
+    val map2 = mutableMapOf("李白" to 1, "杜甫" to 2, "白居易" to 3)
+    var v2 = map2["李白"]
+    println("李白: $v2")
+    map2["李白"] = 100
+    v2 = map2["李白"]
+    println("李白: $v2")
 }
